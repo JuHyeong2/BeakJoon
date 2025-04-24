0bf7e45abb620b16501f87130fbab1e6b6bab19b
JuHyeong2 <100174932+JuHyeong2@users.noreply.github.com>
2025-04-24 22:07:39 +0900

    [Bronze V] Title: 직사각형, Time: 164 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int ver = sc.nextInt();
+        int hor = sc.nextInt();
+
+        System.out.println(ver*hor);
+    }
+}
